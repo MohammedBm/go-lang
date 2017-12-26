64144603262c85e99108207e103d4fcd255f71b1
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2017-12-26 21:15:10 +0300

    -update

--- cards/main.go
+++ cards/main.go
@@ -5,6 +5,8 @@ import (
 )
 
 func main() {
+	// variables
+	/*
 	// var card string = "Ace of Spades"
 	// card := "Ace of Spades"
 	//both of card variables do the same thing
@@ -15,6 +17,20 @@ func main() {
 	card := newCard()
 
 	fmt.Println(card)
+	*/
+
+	//arrays and slice
+	/*
+		arrays can only be of one fixed type, for example they can only be array of strings
+		or an array of integer. You cannot have an array of both types. also array is a fixed length
+		on the other hand, slice can have a different length.complex128
+	*/
+	cards := []string{newCard(), "Five of Hearts"}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
