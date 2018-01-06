0e2f279c687de7dfec5da57b18c7b17930d97e2b
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2018-01-06 18:11:07 +0300

    - finished cards

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -23,4 +24,19 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four Of Clubs" {
 		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
